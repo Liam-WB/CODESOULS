ff8c1b52ec1ccfee5fb34d4680af5b9c1df32d83
LiamWB <liamwartnerblake@gmail.com>
2026-01-02 09:28:43 +0000

    -Add game states, menu - playing -Update game.hpp

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Player.hpp"
+#include "Level.hpp"
 
 class Game
 {
@@ -9,12 +10,29 @@ public:
     void run();
 
 private:
+    enum class State { Menu, Playing };
+
     void processEvents();
     void update(float dt);
     void render();
 
+    void updateMenu(float dt);
+    void updatePlaying(float dt);
+
+    void renderMenu();
+    void renderPlaying();
+
     sf::RenderWindow window;
     sf::Clock clock;
 
+    State state = State::Menu;
+
+    // Menu UI
+    sf::Font font;
+    sf::Text titleText;
+    sf::Text hintText;
+
+    // Gameplay
     Player player;
+    Level level;
 };
